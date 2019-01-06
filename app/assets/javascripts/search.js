98575be9364ee41584c9ac5000768f8f35a044c8
Fippiy <profier4@gmail.com>
2019-01-06 14:55:35 +0900

    Edit search.js

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,6 +1,12 @@
 $(function() {
   $(".search__query").on("keyup", function () {
     var input = $(".search__query").val();
-    console.log(input);
+
+    $.ajax({
+      type: 'GET',
+      url: '/products/search',
+      data: { keyword: input },
+      dataType: 'json'
+    })
   });
 });
